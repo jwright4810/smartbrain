88512f4b381c1e58074dc55a9576dbe2712c6f40
Jarett Wright <jwtech8313@gmail.comgit confit --list>
2019-01-24 21:47:36 -0600

    Updates

---
+++ precache-manifest.0c4e287aec9eca4ab96ca4a690876658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45023ec6664dfab132c6",
+    "url": "/jwright4810/smartbrain.git/static/js/runtime~main.45023ec6.js"
+  },
+  {
+    "revision": "a9ebdde8bd64a310a2b0",
+    "url": "/jwright4810/smartbrain.git/static/js/main.a9ebdde8.chunk.js"
+  },
+  {
+    "revision": "9b040f6877a384037c1d",
+    "url": "/jwright4810/smartbrain.git/static/js/1.9b040f68.chunk.js"
+  },
+  {
+    "revision": "a9ebdde8bd64a310a2b0",
+    "url": "/jwright4810/smartbrain.git/static/css/main.3410f98c.chunk.css"
+  },
+  {
+    "revision": "9b040f6877a384037c1d",
+    "url": "/jwright4810/smartbrain.git/static/css/1.e9af396a.chunk.css"
+  },
+  {
+    "revision": "630af17c6cb620c0b7b5aab7977053b5",
+    "url": "/jwright4810/smartbrain.git/index.html"
+  }
+];
