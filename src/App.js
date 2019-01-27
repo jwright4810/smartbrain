508aa2eedf9eb8e90ed78ba551ab8998ec066d88
Jarett Wright <jwtech8313@gmail.comgit confit --list>
2019-01-26 22:22:39 -0600

    syntax error left local host 3000 in fetch request

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response) {
-          fetch('https://jw-smartbrain-api.herokuapp.com:3000/image', {
+          fetch('https://jw-smartbrain-api.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
